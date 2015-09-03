8a5a4ec595a46052f48c3bdde35712a7cd426b24
Ryan Johnson <rjohnson4444@live.com>
2015-09-03 08:55:18 -0600

    Made a couple tweeks

--- lib/list.rb
+++ lib/list.rb
@@ -1,24 +1,24 @@
-require_relative 'node'  # => true
+require_relative 'node'
 
-class List < Node                            # => Node
-  attr_accessor :head, :tail, :name, :count  # => nil
+class List < Node
+  attr_accessor :head, :tail, :name, :count
   def initialize
-    self.head = nil                          # => nil
-    self.tail = nil                          # => nil
-    @count = 0                               # => 0
-  end                                        # => :initialize
+    self.head = nil
+    self.tail = nil
+    @count = 0
+  end
 
   def head_value
-    return self.head.data  # => "Fred", "Jake"
-  end                      # => :head_value
+    return self.head.data
+  end
 
   def tail_value
     return self.tail.data
-  end                      # => :tail_value
+  end
 
   def count
-    @count   # => 2, 3
-  end        # => :count
+    @count
+  end
 
   def pop
     return nil if self.tail.nil?
@@ -26,18 +26,18 @@ class List < Node                            # => Node
     node = self.tail
     self.tail = self.tail.next_node
     return node
-  end                                # => :pop
+  end
 
   def append(node)
-    @count += 1                   # => 1, 3
-    if self.head.nil?             # => true, false
-      self.head = node            # => #<Node:0x007fb2a09dca60 @data="Fred", @next_node=nil>
-      self.tail = node            # => #<Node:0x007fb2a09dca60 @data="Fred", @next_node=nil>
+    @count += 1
+    if self.head.nil?
+      self.head = node
+      self.tail = node
     else
-      self.tail.next_node = node  # => #<Node:0x007fb2a09dcce0 @data="Ryan", @next_node=nil>
-      self.tail = node            # => #<Node:0x007fb2a09dcce0 @data="Ryan", @next_node=nil>
-    end                           # => #<Node:0x007fb2a09dca60 @data="Fred", @next_node=nil>, #<Node:0x007fb2a09dcce0 @data="Ryan", @next_node=nil>
-  end                             # => :append
+      self.tail.next_node = node
+      self.tail = node
+    end
+  end
 
   def insert(node)
     @count += 1
@@ -46,7 +46,7 @@ class List < Node                            # => Node
     elsif self.head.next_node == nil
       self.head.next_node = node
     end
-  end                                 # => :insert
+  end
 
   def include?(find)
     if self.head.data == find || self.head.next_node.data == find
@@ -56,68 +56,28 @@ class List < Node                            # => Node
     else
       false
     end
-  end                                                              # => :include?
+  end
 
   def prepend(node)
-    @count += 1                   # => 2
-    if self.head.nil?             # => false
+    @count += 1
+    if self.head.nil?
       self.head = node
     else
-      node.next_node = self.head  # => #<Node:0x007fb2a09dca60 @data="Fred", @next_node=nil>
-      self.head = node            # => #<Node:0x007fb2a09dc808 @data="Jake", @next_node=#<Node:0x007fb2a09dca60 @data="Fred", @next_node=nil>>
-    end                           # => #<Node:0x007fb2a09dc808 @data="Jake", @next_node=#<Node:0x007fb2a09dca60 @data="Fred", @next_node=nil>>
-  end                             # => :prepend
+      node.next_node = self.head
+      self.head = node
+    end
+  end
 
   def find_by_value(value)
-    if @count < 1                                          # => false
+    if @count < 1
       false
     else
-      current_node = self.head                             # => #<Node:0x007fb2a09dc808 @data="Jake", @next_node=#<Node:0x007fb2a09dca60 @data="Fred", @next_node=#<Node:0x007fb2a09dcce0 @data="Ryan", @next_node=nil>>>
-      while current_node                                   # => #<Node:0x007fb2a09dc808 @data="Jake", @next_node=#<Node:0x007fb2a09dca60 @data="Fred", @next_node=#<Node:0x007fb2a09dcce0 @data="Ryan", @next_node=nil>>>
-        return current_node if value == current_node.data  # => true
+      current_node = self.head
+      while current_node
+        return current_node if value == current_node.data
         current_node = current_node.next_node
       end
       false
     end
-  end                                                      # => :find_by_value
-end                                                        # => :find_by_value
-
-
-
-node1 = Node.new("Ryan")  # => #<Node:0x007fb2a09dcce0 @data="Ryan", @next_node=nil>
-node2 = Node.new("Fred")  # => #<Node:0x007fb2a09dca60 @data="Fred", @next_node=nil>
-node3 = Node.new("Jake")  # => #<Node:0x007fb2a09dc808 @data="Jake", @next_node=nil>
-list1 = List.new          # => #<List:0x007fb2a09dc5d8 @head=nil, @tail=nil, @count=0>
-#
-list1.append(node2)       # => #<Node:0x007fb2a09dca60 @data="Fred", @next_node=nil>
-
-list1.head_value             # => "Fred"
-list1.prepend(node3)         # => #<Node:0x007fb2a09dc808 @data="Jake", @next_node=#<Node:0x007fb2a09dca60 @data="Fred", @next_node=nil>>
-list1.head_value             # => "Jake"
-list1.count                  # => 2
-list1.append(node1)          # => #<Node:0x007fb2a09dcce0 @data="Ryan", @next_node=nil>
-list1.count                  # => 3
-list1                        # => #<List:0x007fb2a09dc5d8 @head=#<Node:0x007fb2a09dc808 @data="Jake", @next_node=#<Node:0x007fb2a09dca60 @data="Fred", @next_node=#<Node:0x007fb2a09dcce0 @data="Ryan", @next_node=nil>>>, @tail=#<Node:0x007fb2a09dcce0 @data="Ryan", @next_node=nil>, @count=3>
-list1.find_by_value("Jake")  # => #<Node:0x007fb2a09dc808 @data="Jake", @next_node=#<Node:0x007fb2a09dca60 @data="Fred", @next_node=#<Node:0x007fb2a09dcce0 @data="Ryan", @next_node=nil>>>
-
-#
-# list1
-#
-# list1.pop
-# list1
-# list1.pop
-#
-# list1.insert(node1)
-# list1.head_value
-# list1.tail_value
-# node1 = Node.new("Ryan")
-# list1.append(node2)
-# list1
-# list1.append(node3)
-# list1
-# list1.append(node1)
-# list1
-# list1.tail
-# list1
-
-# list1.include?("Ryan")
+  end
+end
